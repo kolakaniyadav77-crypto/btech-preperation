6e29568c3d3186d1d09114fa016d4cff1e7fa75c
bunny <bunnyyadav6493@gmail.com>
2026-02-25 18:43:29 +0530

    fixed dependency conflict

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -30,5 +31,8 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,  // Disable for production
+  },
+  build: {
+    chunkSizeWarningLimit: 1000
   }
 })
